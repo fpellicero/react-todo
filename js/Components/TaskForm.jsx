67b2837ba3304c84b8e0dcb80bdcb47c23386477
Francesc Pellicero <francesc@tiendeo.com>
2017-03-17 09:32:28 +0100

    Añadido formulario crear tarea

---
+++ js/Components/TaskForm.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {Form, FormGroup, FormControl, Button} from 'react-bootstrap';
+
+class TaskForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this._onClick = this._onClick.bind(this);
+  }
+
+  _onClick(e) {
+    e.preventDefault();
+    let task = ReactDOM.findDOMNode(this.input).value;
+    
+    if(typeof this.props.onInput === "function") this.props.onInput(task);
+  }
+
+  render() {
+    return (
+      <Form inline>
+        <FormGroup controlId="formInlineName">
+          <FormControl
+            type="text"
+            placeholder="What's next?"
+            ref={(input) => {this.input = input;}} />
+        </FormGroup>
+        <Button type="submit" onClick={this._onClick}>
+          Go!
+        </Button>
+      </Form>
+    )
+  }
+}
+
+export default TaskForm;
--- js/Components/TodoApp.jsx
+++ js/Components/TodoApp.jsx
@@ -1,29 +1,40 @@
 import React, {Component} from 'react';
 import Title from './Title';
 import TaskList from './TaskList';
+import TaskForm from './TaskForm';
 import Grid from 'react-bootstrap/lib/Grid';
+
 
 class TodoApp extends Component {
   constructor(props) {
     super(props);
 
     this.state = this._getInitialState();
+
+    this._createTask = this._createTask.bind(this);
   }
 
   _getInitialState() {
     return {
-      tasks: [
-        'Buy gummies',
-        'Conquer the world',
-        'Find a cow with wheels'
-      ]
+      tasks: []
     }
+  }
+
+  _createTask(task) {
+    let tasks = this.state.tasks.slice();
+
+    tasks.push(task);
+
+    this.setState({
+      tasks: tasks
+    })
   }
 
   render() {
     return(
       <Grid>
         <Title title="My ToDo App" />
+        <TaskForm onInput={this._createTask}/>
         <TaskList tasks={this.state.tasks} />
       </Grid>
     )
