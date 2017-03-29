a70b1f8fc15b0156cd83cbf87140b59cad655d27
robin2309 <robin.besson@tiendeo.com>
2017-03-29 11:19:21 +0200

    button add subtask

--- js/Components/Task.jsx
+++ js/Components/Task.jsx
@@ -1,11 +1,31 @@
+import React, {Component} from 'react';
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
+import {Button} from 'react-bootstrap';
 
-const Task = (props) => {
-  return (
-    <ListGroupItem>
-      {props.task}
-    </ListGroupItem>
-  )
+class Task extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  _onClick() {
+    console.log('clicked');
+  }
+
+  render() {
+    const style = {
+      position: 'absolute',
+      right: '3px',
+      top: '3px'
+    };
+    return (
+      <ListGroupItem>
+        {this.props.task}
+        <Button type="submit" onClick={this._onClick} style={style}>
+          Add subtask
+        </Button>
+      </ListGroupItem>
+    )
+  }
 }
 
 export default Task;
